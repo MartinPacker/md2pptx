bc345646f271a87c55f01a0a92bbb1582350b9ce
MartinPacker <martin_packer@uk.ibm.com>
2024-01-07 17:25:45 +0000

    Card class

---
+++ card.py
@@ -0,0 +1,16 @@
+"""
+card
+"""
+
+myVersion = "0.0"
+
+__version__ = myVersion
+
+class Card:
+    def __init__(
+        self,
+    ):
+        self.title = ""
+        self.bullets = ""
+        self.graphic = ""
+
